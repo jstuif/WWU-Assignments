02cabf488c7ff715125ba466c278a689fb2f377a
Joshua Stuifbergen <47699301+jstuif@users.noreply.github.com>
2020-06-09 16:55:17 -0700

    Create Exercise1.c

---
+++ CISS-247/Reverse_Engineer/Exercise1.c
@@ -0,0 +1,21 @@
+/*
+Joshua Stuifbergen
+12/5/18
+CISS247
+Lab 8 Exercise 1
+*/
+
+#include <stdio.h>
+
+int main (){
+
+	int x, y;
+	
+	x = 0;
+	y = 0;
+	
+	y = x + y;
+	x = x + y;
+	y = x++ + y;
+ 	
+}
