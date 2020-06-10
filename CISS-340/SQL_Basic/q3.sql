161e17abd1018b2d367399660a3215d1f4c35d74
Joshua Stuifbergen <47699301+jstuif@users.noreply.github.com>
2020-06-09 17:01:47 -0700

    Create q3.sql

---
+++ CISS-340/SQL_Basic/q3.sql
@@ -0,0 +1,3 @@
+# Display the first name and last name of employees sorted based on their age (listing the youngest employees first)
+
+SELECT first_name, last_name FROM employees ORDER BY birth_date DESC
